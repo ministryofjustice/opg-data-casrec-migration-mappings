66b46bcb67a7b1daf08027fdf9a084902b1236bb
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-06-08 15:12:49 +0100

    new defs

--- app/run.py
+++ app/run.py
@@ -24,10 +24,10 @@ def loop_through_files():
     remove_old_files()
 
     for mapping_file in os.listdir(config['SPREADSHEET_PATH']):
-        print(f"mapping_file: {mapping_file}")
         # if mapping_file == 'Casrec_Mapping_Document_Bond.xlsx':
         if os.path.isfile(os.path.join(config['SPREADSHEET_PATH'], mapping_file)):
             if mapping_file[:2] != '~$':
+                print(f"mapping_file: {mapping_file}")
                 entity_name = get_entity_name_from_file(file_name=mapping_file)
                 entity_file_path = create_entity_folder(entity_name=entity_name)
                 generate_files(spreadsheet_name=mapping_file, destination=entity_file_path)
